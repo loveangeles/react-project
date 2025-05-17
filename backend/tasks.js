3623ae428b24f5b1a23d55e141c4de53c409363a
949936324@qq.com <18384113+loveangeles@users.noreply.github.com>
2025-05-18 03:57:23 +0800

    first commit

---
+++ backend/tasks.js
@@ -0,0 +1,24 @@
+ module.exports.TaskList  = {
+    code: 0,
+    codeText: "OK",
+    page: 1,
+    pageNum: 1,
+    total: 2,
+    limit: 100,
+    list: [
+        {
+            id: 1,
+            task: "工作1",
+            state: 2,
+            time: "2025-05-15 09:16:16",
+            complete: "2025-05-15 20:17:18"
+        },
+        {
+            id: 2,
+            task: "工作2",
+            state: 2,
+            time: "2025-05-15 00:16:16",
+            complete: "2025-05-15 00:17:18"
+        }
+    ]
+}
